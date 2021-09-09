275adce21fe85aaa8c8fe014a15e5a66a1bc8bf6
Tóth András <tothand95@gmail.com>
2021-09-09 22:56:45 +0200

    feleslegesek törlése, újak felvétele

--- TodoApp/TodoApp.Web/ClientApp/src/app/app.module.ts
+++ TodoApp/TodoApp.Web/ClientApp/src/app/app.module.ts
@@ -7,16 +7,22 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ChangePasswordComponent } from './change-password/change-password.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoAddComponent } from './todo-add/todo-add.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent
+    LoginComponent,
+    RegisterComponent,
+    ChangePasswordComponent,
+    TodoListComponent,
+    TodoAddComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -24,8 +30,10 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent },
+      { path: 'change-password', component: ChangePasswordComponent },
+      { path: 'todos', component: TodoListComponent },
     ])
   ],
   providers: [],
