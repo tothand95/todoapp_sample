36e2c8bb65d22af84e793a1a325854fb008da68a
Tóth András <tothand95@gmail.com>
2021-09-09 22:04:17 +0200

    auth manager interface

--- TodoApp/TodoApp.Bll/Managers/Interfaces/IAuthManager.cs
+++ TodoApp/TodoApp.Bll/Managers/Interfaces/IAuthManager.cs
@@ -5,10 +5,16 @@ using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Identity;
 
+using TodoApp.Bll.Dtos;
+
 namespace TodoApp.Bll.Managers
 {
     public interface IAuthManager
     {
         Task<SignInResult> LoginAsync(string username, string password);
+        Task<IdentityResult> AddUserAsync(RegisterUserDto dto);
+        Task<IdentityResult> ChangePasswordAsync(string username, string currentPassword, string newPassword);
+        Task<List<string>> GetRolesForUserAsync(string username);
+        Task<bool> HasRoleAsync(string username, string role);
     }
 }
