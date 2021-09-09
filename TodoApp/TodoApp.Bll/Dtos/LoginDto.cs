2a22261c5ead5cf67b80b079613038621cb91a6b
Tóth András <tothand95@gmail.com>
2021-09-09 22:05:39 +0200

    login api

---
+++ TodoApp/TodoApp.Bll/Dtos/LoginDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoApp.Bll.Dtos
+{
+    public class LoginDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ TodoApp/TodoApp.Web/Controllers/AuthController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+
+using TodoApp.Bll.Dtos;
+using TodoApp.Bll.Managers;
+
+namespace TodoApp.Web.Controllers
+{
+    [Produces("application/json")]
+    [Consumes("application/json")]
+    [Route("api/user")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public IAuthManager AuthManager { get; set; }
+
+        public AuthController(IAuthManager authManager)
+        {
+            AuthManager = authManager;
+        }
+
+
+        [HttpPost, Route("login")]
+        public async Task<IActionResult> LoginAsync([FromBody] LoginDto user)
+        {
+            if (user == null)
+            {
+                return BadRequest("Request can't be completed. Internal server error.");
+            }
+
+            var response = await AuthManager.LoginAsync(user.Username, user.Password);
+
+            if (response.Succeeded)
+            {
+                var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("superSecretKey@345"));
+                var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+                var roles = await AuthManager.GetRolesForUserAsync(user.Username);
+
+                var claimList = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, user.Username),
+                };
+                foreach (var role in roles)
+                    claimList.Add(new Claim(ClaimTypes.Role, role));
+
+                var tokenOptions = new JwtSecurityToken(
+                    issuer: "http://localhost:44353",
+                    audience: "http://localhost:44353",
+                    claims: claimList,
+                    expires: DateTime.Now.AddMinutes(60),
+                    signingCredentials: signinCredentials
+                );
+
+                var tokenString = new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+                return Ok(new { Token = tokenString });
+            }
+            else
+            {
+                return BadRequest("Login failed. Invalid username or password.");
+            }
+        }
+    }
+}
