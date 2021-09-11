51bf26a12e816fed41485fc039aa0a5899445a5e
Tóth András <tothand95@gmail.com>
2021-09-11 13:23:25 +0200

    todo list modalhoz

--- TodoApp/TodoApp.Web/ClientApp/src/app/components/todo-list/todo-list.component.ts
+++ TodoApp/TodoApp.Web/ClientApp/src/app/components/todo-list/todo-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 import { TodoService } from 'src/app/services/todo.service';
 import { TodoModel } from 'src/model/todo-model';
@@ -9,17 +9,24 @@ import { TodoModel } from 'src/model/todo-model';
   styleUrls: ['./todo-list.component.css']
 })
 export class TodoListComponent implements OnInit {
+  @Input() userId: string;
   todos: TodoModel[] = [];
 
   constructor(private todoService: TodoService, private authService: AuthService) { }
 
   ngOnInit() {
-    console.log('oninit');
-    this.todoService.listTodoForCurrentUser()
-      .subscribe(response => {
-        this.todos = response;
-      }, err => {
-      });
+    if (this.userId === null) {
+      this.todoService.listTodoForCurrentUser()
+        .subscribe(response => {
+          this.todos = response;
+        }, err => {
+        });
+    } else {
+      this.todoService.listTodoForUser(this.userId, false)
+        .subscribe(response => {
+          this.todos = response;
+        }, err => {
+        });
+    }
   }
-
 }
